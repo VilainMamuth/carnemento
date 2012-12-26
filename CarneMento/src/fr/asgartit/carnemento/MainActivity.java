6083fe208a55b3f1d0f2193e16ab63b488102390
Emmanuel Da Mota <emmanuel.damota@gmail.com>
2012-12-26 15:46:18 +0100

    first release

--- CarneMento/src/fr/asgartit/carnemento/MainActivity.java
+++ CarneMento/src/fr/asgartit/carnemento/MainActivity.java
@@ -1,15 +1,22 @@
 package fr.asgartit.carnemento;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		
+		String[] p = getResources().getStringArray(R.array.principes);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, p);
+		
+		setListAdapter(adapter);
 	}
 
 	@Override
